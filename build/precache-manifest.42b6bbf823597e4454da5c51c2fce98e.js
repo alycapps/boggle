bfab5e931ff37afeef206f00bd3bde97c1b7283c
Alyssa Capps <alyssancapps@gmail.com>
2020-08-24 21:31:08 -0400

    initial react app

---
+++ build/precache-manifest.42b6bbf823597e4454da5c51c2fce98e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a97b34a5b4090b22a4954b9c2d2a8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b86cdacae8e78d525c2e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "808c40a0e2614fdddeff",
+    "url": "/static/js/2.1f11caf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f11caf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86cdacae8e78d525c2e",
+    "url": "/static/js/main.134d8618.chunk.js"
+  },
+  {
+    "revision": "6acd1ce5479823c8a8b6",
+    "url": "/static/js/runtime-main.c5badfb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
