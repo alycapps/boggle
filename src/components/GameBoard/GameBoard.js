507f33d35fae04e7e6305d3dbf8defe4cf8b1f69
Alyssa Capps <alyssancapps@gmail.com>
2020-09-03 20:09:26 -0400

    added gameboard component

---
+++ src/components/GameBoard/GameBoard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+var borderStyle = {
+    border: '2px solid black',
+    borderRadius: "25px"
+  };
+
+
+export const GameBoard = (props) => (
+    <div>
+         <br/>
+        <Container>
+          <Row>
+              <Col md={3}>
+                hello
+              </Col>
+              <Col md={6} style={borderStyle}>
+                {props.children}
+              </Col>
+              <Col md={3}>
+                hello
+              </Col>
+          </Row>
+        </Container>
+    </div>
+)
+
+export default GameBoard;
--- src/pages/Play/Play.js
+++ src/pages/Play/Play.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import GameBoard from "../../components/GameBoard"
+
 
 const Play = () => (
     <div>
         <h1>Play page</h1>
+        <GameBoard>
+            <div>4 or 5 rows of dice placeholder</div>
+        </GameBoard>
     </div>
 );
 
