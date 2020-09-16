dff7e3eff98b294f03b634508878c517f746ce46
Alyssa Capps <alyssancapps@gmail.com>
2020-09-15 20:44:32 -0400

    setting up mysql basics

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
+// Loading evnironmental variables here
+if (process.env.NODE_ENV !== 'production') {
+	console.log('loading dev environments');
+	require('dotenv').config();
+}
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const mysql = require('mysql');
 const PORT = process.env.PORT || 3001;
-const routes = require("./routes");
+// const routes = require("./routes");
+
+// app.use(function(req, res, next) {
+// res.header('Access-Control-Allow-Origin', '*');
+// res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+// next();
+// });
 
 app.set('port', process.env.PORT || 3001);
 
@@ -17,8 +30,25 @@ app.get('/', (req, res) => {
 });
 }
 
+// config db ====================================
+const connection = mysql.createConnection({
+  host: process.env.dbHost,
+  user: process.env.dbUser,
+  password: process.env.dbPass,
+  port: '3306',
+  database: process.env.dbName
+});
+
+// connect to db 
+connection.connect(function(err) {
+	if (err) {
+		return console.error('error: ' + err.message);
+	}
+	console.log('Connected to the MySQL server.');
+});
+
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
 // Error handler
 app.use(function(err, req, res, next) {
@@ -29,6 +59,8 @@ app.use(function(err, req, res, next) {
 
 // Starting Server
 app.listen(PORT, '0.0.0.0', (err) => {
+	if (err) {
+		return console.error('error: ' + err.message);
+	}
 	console.log(`API Server now listening on PORT ${PORT}!`);
-	console.log(err);
 });
