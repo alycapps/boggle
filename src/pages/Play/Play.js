a33c402ef1dea8373b9faaca6d9c392e86df04f2
Alyssa Capps <alyssancapps@gmail.com>
2020-09-28 22:52:43 -0400

    added state to play component

--- src/pages/Play/Play.js
+++ src/pages/Play/Play.js
@@ -1,16 +1,49 @@
-import React from 'react';
-import GameBoard from "../../components/GameBoard"
-import Die from "../../components/Die"
+import React, { Component } from "react";
+import { withRouter } from 'react-router-dom';
+import GameBoard from "../../components/GameBoard";
+import Die from "../../components/Die";
 
 
-const Play = () => (
-    <div>
-        <h1>Play page</h1>
-        <GameBoard>
-            <Die value={"A"}>
-            </Die>
-        </GameBoard>
-    </div>
-);
+// const Play = () => (
+class Play extends Component {
+    state = {
+        dice: {
+            "1":["a","b","c","d","e","f"],
+            "2":["a","b","c","d","e","f"],
+            "3":["a","b","c","d","e","f"],
+            "4":["a","b","c","d","e","f"],
+            "5":["a","b","c","d","e","f"],
+            "6":["a","b","c","d","e","f"],
+            "7":["a","b","c","d","e","f"],
+            "8":["a","b","c","d","e","f"],
+            "9":["a","b","c","d","e","f"],
+            "11":["a","b","c","d","e","f"],
+            "12":["a","b","c","d","e","f"],
+            "13":["a","b","c","d","e","f"],
+            "14":["a","b","c","d","e","f"],
+            "15":["a","b","c","d","e","f"],
+            "16":["a","b","c","d","e","f"],
+        }
+    };
+    componentDidMount() {
+        this.randomizeDice()
+    };
+    randomizeDice() {
+        console.log("gonna add in logic to chose value for each die")
+    };
 
-export default Play;
+    render() {
+        return (
+            <div>
+                <h1>Play page</h1>
+                <GameBoard>
+                    <Die value={"A"}>
+                    </Die>
+                </GameBoard>
+            </div>
+        )
+    }
+};
+
+// export default Play;
+export default withRouter(Play);
