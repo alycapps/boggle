37f4d65ef3851831c6d1ed0bedbd505bfefed18f
Alyssa Capps <alyssancapps@gmail.com>
2021-01-25 21:26:27 -0500

    updated textform

--- src/components/textForm/index.js
+++ src/components/textForm/index.js
@@ -1,1 +1,18 @@
-export { default } from "./TextForm";
+import React from "react";
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const textForm = (props) => (
+    <div>
+         <Form>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Example textarea</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </Form>
+    </div>
+);
+
+export default textForm;
