745b4adb03293394bd6ca3900aaaf9d36d907c49
Alyssa Capps <alyssancapps@gmail.com>
2021-01-25 21:29:37 -0500

    updated HeadNav

--- src/components/HeadNav/index.js
+++ src/components/HeadNav/index.js
@@ -1,1 +1,14 @@
-export { default } from "./HeadNav";
+import React from "react";
+import { Navbar, Nav } from 'react-bootstrap';
+
+const HeadNav = (props) => (
+    <Navbar bg="primary" variant="dark">
+      <Navbar.Brand href="#home">Boggle</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#profile">Profile</Nav.Link> 
+        <Nav.Link href="#login">Login</Nav.Link>
+      </Nav>
+    </Navbar> 
+);
+
+export default HeadNav;
