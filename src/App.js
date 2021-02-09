a45ccd6f82dd100bb2683fbafa4f6ba8e4bd50b5
Alyssa Capps <alyssancapps@gmail.com>
2021-02-08 21:05:55 -0500

    fixing file setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Play from "./pages/Play.js"
-import NoMatch from "./pages/NoMatch"
+import NoMatch from "./pages/NoMatch.js"
 import Nav from "./components/HeadNav.js"
 
 function App() {
